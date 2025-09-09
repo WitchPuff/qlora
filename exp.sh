0d2484c34a92319e0b2aa9abea8c4ecbad04a38a
桐 <witchpuff@Lacklustar.local>
2025-09-09 02:19:02 +0200

    fix main

--- exp.sh
+++ exp.sh
@@ -10,4 +10,4 @@
 /home/tongtong/miniconda3/envs/mb/bin/python main.py --model_name roberta-base --dataset sst2 --r 4 --lora_alpha 16 --target_modules query value --learning_rate 1e-3 --epochs 25 --batch_size 256 --weight_decay 1e-2 &&
 /home/tongtong/miniconda3/envs/mb/bin/python main.py --model_name roberta-base --dataset trec --r 4 --lora_alpha 16 --target_modules query value --learning_rate 1e-3 --epochs 25 --batch_size 256 --weight_decay 1e-2 &&
 /home/tongtong/miniconda3/envs/mb/bin/python main.py --model_name roberta-base --dataset sst2 --r 16 --lora_alpha 16 --target_modules query value --learning_rate 1e-3 --epochs 25 --batch_size 256 --weight_decay 1e-2 &&
-/home/tongtong/miniconda3/envs/mb/bin/python main.py --model_name roberta-base --dataset trec --r 16 --lora_alpha 16 --target_modules query value --learning_rate 1e-3 --epochs 25 --batch_size 256 --weight_decay 1e-2 &&
+/home/tongtong/miniconda3/envs/mb/bin/python main.py --model_name roberta-base --dataset trec --r 16 --lora_alpha 16 --target_modules query value --learning_rate 1e-3 --epochs 25 --batch_size 256 --weight_decay 1e-2
